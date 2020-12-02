2dc0a2ef6e177600a6f1c6c45492745b938e5058
Harold Arias <Harold_Arias@DESKTOP-EI5PVVA>
2020-12-02 00:13:28 -0500

    Actualización_Master

---
+++ VirtualGym/src/main/java/tecsup/edu/pe/virtualgym/VirtualGymApplication.java
@@ -0,0 +1,13 @@
+package tecsup.edu.pe.virtualgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualGymApplication.class, args);
+	}
+
+}
